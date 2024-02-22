1c8a333f0998f852af309c60190769817c1b57dc
Gia Chí Đặng <danggiachi@gmail.com>
2024-02-22 22:40:15 +0700

    Added requirements.txt

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template
+# from flask import Blueprint, render_template
 
-views = Blueprint(__name__, "views")
+# views = Blueprint(__name__, "views")
 
-@views.route("/")
-def home():
-    return render_template("index.html")
+# @views.route("/")
+# def home():
+#     return render_template("index.html")
