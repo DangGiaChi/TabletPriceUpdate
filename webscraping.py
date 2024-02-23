bfca736a46783b350ad909218da3c209c1d459d6
Gia Chí Đặng <danggiachi@gmail.com>
2024-02-23 19:26:14 +0700

    Changed webscraping to library, added Tab S9+ 5G

--- webscraping.py
+++ webscraping.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
+import numpy as np
+from pywebio.output import *
+
 def Extract_CellphoneS(url = None):
     page_to_scrape = requests.get(url)
     soup = BeautifulSoup(page_to_scrape.text, "html.parser")
@@ -16,8 +19,10 @@ def Extract_CellphoneS(url = None):
     for i in data:
         product.append(i.find("strong").text)
         price.append(i.find("span").text)
+    
+    shop = ["cellphoneS"] * len(product)
 
-    return product, price
+    return np.c_[shop, product, price]
 
 def Extract_Samcenter(url = None):
     page_to_scrape = requests.get(url, verify=False)
@@ -36,30 +41,17 @@ def Extract_Samcenter(url = None):
     product = soup.find(product_selector)
     product = product.text
 
-    return product, price
+    shop = "SamCenter"
 
-product_tab_s9, price_tab_s9 = Extract_CellphoneS("https://cellphones.com.vn/samsung-galaxy-tab-s9-wifi.html")
-for pro, pri in zip(product_tab_s9, price_tab_s9):
-    print(pro + " - " + pri)
+    return [shop, product, price]
 
-print("-------------------------------")
-
-product_tab_s9_plus, price_tab_s9_plus = Extract_CellphoneS("https://cellphones.com.vn/samsung-galaxy-tab-s9-plus-wifi-512gb.html")
-for pro, pri in zip(product_tab_s9_plus, price_tab_s9_plus):
-    print(pro + " - " + pri)   
-
-print("-------------------------------")
-
-product_ipad_air_5, price_ipad_air_5 = Extract_CellphoneS("https://cellphones.com.vn/ipad-air-5.html")
-for pro, pri in zip(product_ipad_air_5, price_ipad_air_5):
-    print(pro + " - " + pri)   
-
-print("-------------------------------")
-
-sproduct_tab_s9, sprice_tab_s9 = Extract_Samcenter("https://samcenter.vn/vn/galaxy-tab-s9-wi-fi-128gb")
-print(sproduct_tab_s9 + " - " + sprice_tab_s9)
-
-print("-------------------------------")
-
-sproduct_tab_s9_plus, sprice_tab_s9_plus = Extract_Samcenter("https://samcenter.vn/vn/galaxy-tab-s9-plus-wifi-256gb")
-print(sproduct_tab_s9_plus + " - " + sprice_tab_s9_plus)
+def make_table(url = None):
+        table = [["Shop", "Product", "Price"]]
+        if "cellphone" in url:
+            data = Extract_CellphoneS(url)
+            for item in data:
+                table.append(item)
+        else:
+            data = Extract_Samcenter(url)
+            table.append(data)
+        put_table(table)
